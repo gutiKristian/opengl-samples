170b5d840f7e688f762625a24c623f0c4625a003
Kristián Gutič <kristian.gutic@gmail.com>
2023-06-27 18:48:04 +0200

    Adding ShaderProgram

---
+++ Dunno/glutil/ShaderProgram.cpp
@@ -0,0 +1,37 @@
+//
+// Created by krist on 27. 6. 2023.
+//
+
+#include "ShaderProgram.h"
+#include "core/Log.h"
+namespace Dunno
+{
+    ShaderProgram::ShaderProgram(const std::filesystem::path& vertexShader, const std::filesystem::path& fragmentShader)
+        :ShaderProgram(Shader(vertexShader, GL_VERTEX_SHADER),
+                       Shader(fragmentShader, GL_FRAGMENT_SHADER))
+    {
+    }
+
+    ShaderProgram::ShaderProgram(Shader vertex, Shader fragment)
+        :ShaderProgram()
+    {
+        if (!mProgram)
+        {
+            LOG_ERROR("Error while creating ShaderProgram, invalid mProgram!!!");
+        }
+
+        mShaders.push_back(vertex);
+        mShaders.push_back(fragment);
+
+        // Check if GetId returns non-zero, then proceed? -- we already log such thing inside Shader
+
+        glAttachShader(mProgram, vertex.GetId());
+        glAttachShader(mProgram, fragment.GetId());
+    }
+
+    ShaderProgram::ShaderProgram()
+    {
+        mProgram = glCreateProgram();
+    }
+
+}
