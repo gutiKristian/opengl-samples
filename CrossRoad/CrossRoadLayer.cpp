ca0f33af4fc770eb7acbfd728b7aae4aca8f6159
Kristián Gutič <kristian.gutic@gmail.com>
2023-06-27 12:45:27 +0200

    Adding new layer

---
+++ CrossRoad/CrossRoadLayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by krist on 27. 6. 2023.
+//
+
+#include "CrossRoadLayer.h"
---
+++ CrossRoad/CrossRoadLayer.h
@@ -0,0 +1,15 @@
+//
+// Created by krist on 27. 6. 2023.
+//
+
+#ifndef OPENGL_SAMPLES_CROSSROADLAYER_H
+#define OPENGL_SAMPLES_CROSSROADLAYER_H
+
+
+class CrossRoadLayer
+{
+
+};
+
+
+#endif //OPENGL_SAMPLES_CROSSROADLAYER_H
